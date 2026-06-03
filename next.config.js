f5d946f39ef26dfb4b496fdc3db0fa23c5c915a4
Alula <git@alula.me>
2026-06-04 01:25:21 +0200

    Fix cloudflare build

--- next.config.js
+++ next.config.js
@@ -3,22 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export", // Static HTML export
   images: {
-		loader: "custom",
-		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-	},
-	transpilePackages: ["next-image-export-optimizer"],
-	env: {
-		nextImageExportOptimizer_imageFolderPath: "public/img",
-		nextImageExportOptimizer_exportFolderPath: "out",
-		nextImageExportOptimizer_quality: 75,
-		nextImageExportOptimizer_storePicturesInWEBP: true,
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  transpilePackages: ["next-image-export-optimizer"],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: "public/img",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: 75,
+    nextImageExportOptimizer_storePicturesInWEBP: true,
 
-		// If you do not want to use blurry placeholder images, then you can set
-		// nextImageExportOptimizer_generateAndUseBlurImages to false and pass
-		// `placeholder="empty"` to all <ExportedImage> components.
-		nextImageExportOptimizer_generateAndUseBlurImages: true,
-	},
+    // If you do not want to use blurry placeholder images, then you can set
+    // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
+    // `placeholder="empty"` to all <ExportedImage> components.
+    nextImageExportOptimizer_generateAndUseBlurImages: true,
+  },
   transpilePackages: ["next-mdx-remote"],
   experimental: {
     webpackBuildWorker: true,
